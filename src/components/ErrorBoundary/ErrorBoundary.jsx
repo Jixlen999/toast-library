719529803c728ee0d5091afe98c7a2d90f29dd49
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-11-17 04:00:01 +0300

    feat: create ErrorBoundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return (
+        <div>
+          <h1>Oops, somebody stole your notification :o</h1>
+        </div>
+      );
+    }
+
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = PropTypes.node.isRequired;
+
+export default ErrorBoundary;
